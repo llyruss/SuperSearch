aa8bd664d498e26d22a7cfcc615ef2c7b691682b
gmun <garrettohrt@gmail.com>
2022-10-07 13:48:32 -0500

    add imdb api, links in results html, html for films and comics

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,7 +11,37 @@ fetch(requestUrl)
        console.log(data)
     });
 
+//var searchButton = document.querySelector("search-button")
 
+//function getApi() {
+
+   // var userSearch = document.querySelector("#hero-name").value
+//console.log(userSearch)
+    
+   // var searchParams = "&title_type=feature,tv_series&keywords=superhero"
+    //var requestUrl = "https://imdb-api.com/API/AdvancedSearch/k_zzx14p64?title="+userSearch+searchParams;
+    var featContainer = document.getElementById('hero-feature');
+var requestUrl = "https://imdb-api.com/API/AdvancedSearch/k_zzx14p64?title=black panther&title_type=feature,tv_series&keywords=superhero"
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+
+        var movieTitle = document.createElement('h3');
+        var moviePoster = document.createElement('figure');
+
+        movieTitle.textContent = data.results[0].title;
+        moviePoster.appendChild(document.createElement('img'));
+        moviePoster.src = data.results[0].image;
+        
+        featContainer.append(movieTitle);
+        featContainer.append(moviePoster);
+
+      
+    })
 
 
 
